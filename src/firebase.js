6f88cf4250875a89f3e23b3425f7012d7681d135
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-09-21 21:58:14 -0400

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCY5IHZswM4KMho96SSFJT8TuWPBHAbmaA",
+  authDomain: "clone-ebe87.firebaseapp.com",
+  projectId: "clone-ebe87",
+  storageBucket: "clone-ebe87.appspot.com",
+  messagingSenderId: "481623002400",
+  appId: "1:481623002400:web:c25ea8a568f069c395db45",
+  measurementId: "G-2Q3FR9ZTWK"
+};
