c7f719420042c4dc579d2d455d471d174cd8aa4f
Rauf Maharramov <raufmaharramov@Raufs-MacBook-Pro.local>
2021-09-30 18:07:04 -0400

    front_end

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Login from "./Login";
 import { useEffect } from "react";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe("pk_test_51JfVSNBn07VFZzXJ1qCCbXwuTVsGBmVarev9O152qkBFQhhKixHbFbTU2djwgnBmn049yCgeJEe8L8ffDjjKOaGq00YrrL7uaU");
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -31,15 +36,22 @@ function App() {
     // BEM convention
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/Payment.js
@@ -0,0 +1,135 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import CurrencyFormat from "react-currency-format";
+import { Link } from "react-router-dom";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Payment.css";
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [clientSecret, setClientSecret] = useState(true);
+
+  // This is the heart of payment >>>>>>>
+  useEffect(() => {
+    // generate the special stripe secret which allows us to charge a customer
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        // Stripe expects the total in a currencies subunits
+        url: `/payment/create?total=${getBasketTotal(basket) * 100}`
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+    getClientSecret();
+  }, [basket]);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async e => {
+    // Stripe functinality
+    e.preventDefault();
+    // setting processing true will trigger disabled in button and it will prevent you clicking the pay button multiple times
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement)
+        }
+      })
+      .then(({ paymentIntent }) => {
+        // paymentIntent = payment confirmation
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+        // history.replace we do because we don't wont
+        // them to be able to go back to the same payment page after payment is done
+        history.replace("/orders");
+      });
+  };
+
+  const handleChange = e => {
+    // Listen for changes in the CardElement
+    // and display and errors as the customer types their card details
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+        {/* Payment section - Delivery address */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        {/* Payment section - Review items */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {/* Products to buy */}
+            {basket.map(item => (
+              <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
+            ))}
+          </div>
+        </div>
+
+        {/* Payment section - Payment method */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/* Stripe method goes here */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={value => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {/* if there is any error with the card it will pop up */}
+              {error && <div>{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
